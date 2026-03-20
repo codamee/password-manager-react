8474bb829fd2e26b893816fdde22ccbd3917fc71
naveen <185893220+codamee@users.noreply.github.com>
2026-03-20 09:14:48 +0530

    style:add the hover effect to nav LI's

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
   return (
       <div className='px-3 py-2 md:py-4 md:px-10 flex justify-between items-center  '>
           <div className="logo md:text-xl font-bold text-green-500">&lt;Pass <span className=' text-blue-500'>mngr/&gt;</span></div>
-          <ul className='flex md:gap-6 gap-4 text-[.8rem] md:text-lg text-slate-500 font-semibold'>
-              <li className='cursor-pointer'>Home</li>
-              <li className='cursor-pointer'>Contact</li>
-              <li className='cursor-pointer'>AboutUs</li>
+          <ul className='flex md:gap-6 gap-4 text-[.8rem] md:text-lg text-slate-400 font-semibold'>
+              <li className='cursor-pointer hover:text-slate-300'>Home</li>
+              <li className='cursor-pointer hover:text-slate-300'>Contact</li>
+              <li className='cursor-pointer hover:text-slate-300'>AboutUs</li>
           </ul>
     </div>
   )
